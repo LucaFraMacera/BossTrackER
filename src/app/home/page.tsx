0b463674c0a7541c9579c6e2129840b289864873
LucaFraMac <lucamacera02@gmail.com>
2024-08-07 16:36:48 +0200

    remodeled home page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -21,36 +21,45 @@ export default function Home(){
 
 
     return <div className={styles["stat-page"]}>
-        <div className={styles["stat-box"]}>
-            <h1>Next Boss</h1>
-            <div className={styles["stat-box-info"]}>
-                <Attribute className={styles["stat-attribute"]} title={"Boss name"} text={nextBossToKill?.name || ""}/>
-                <Attribute className={styles["stat-attribute"]} title={"Boss location"} text={`${nextBossToKill?.region}, ${nextBossToKill?.location}`}/>
-                <Attribute className={styles["stat-attribute"]} title={"Rewards"} text={nextBossToKill && nextBossToKill.drops.length > 0 ? arrayToString(nextBossToKill?.drops!) : "Nothing"}/>
+        <div className={styles.generals}>
+            <div className={styles["stat-box"]}>
+                <h1>Next Boss</h1>
+                <div className={styles["stat-box-info"]}>
+                    <Attribute className={styles["stat-attribute"]} title={"Boss name"}
+                               text={nextBossToKill?.name || ""}/>
+                    <Attribute className={styles["stat-attribute"]} title={"Boss location"}
+                               text={`${nextBossToKill?.region}, ${nextBossToKill?.location}`}/>
+                    <Attribute className={styles["stat-attribute"]} title={"Rewards"}
+                               text={nextBossToKill && nextBossToKill.drops.length > 0 ? arrayToString(nextBossToKill?.drops!) : "Nothing"}/>
+                </div>
+                <BossLink className={"bossLink"} bossId={nextBossToKill?.id!} text={"Check map"}/>
             </div>
-            <BossLink className={"bossLink"} bossId={nextBossToKill?.id!} text={"Check map"}/>
-        </div>
-        <div className={styles["stat-box"]}>
-            <h1>General Stats</h1>
-            <div className={styles["stat-box-info"]}>
-                <Attribute className={styles["stat-attribute"]} title={"Total bosses defeated"} text={`${totalDefeatedBosses} / ${totalBosses}`}/>
-                <Attribute className={styles["stat-attribute"]} title={"Total deaths"} text={`${totalDeaths}`}/>
-                <Attribute className={styles["stat-attribute"]} title={"Most tried boss"} text={mostDifficultBoss ? `${mostDifficultBoss.name}, ${mostDifficultBoss.tries}` : "You haven't died to a boss yet"}/>
+            <div className={styles["stat-box"]}>
+                <h1>General Stats</h1>
+                <div className={styles["stat-box-info"]}>
+                    <Attribute className={styles["stat-attribute"]} title={"Total bosses defeated"}
+                               text={`${totalDefeatedBosses} / ${totalBosses}`}/>
+                    <Attribute className={styles["stat-attribute"]} title={"Total deaths"} text={`${totalDeaths}`}/>
+                    <Attribute className={styles["stat-attribute"]} title={"Most tried boss"}
+                               text={mostDifficultBoss ? `${mostDifficultBoss.name}, ${mostDifficultBoss.tries}` : "You haven't died to a boss yet"}/>
+                </div>
+                <Link className={"bossLink"} href={"/home/stats"}>Check all</Link>
             </div>
-            <Link className={"bossLink"} href={"/home/stats"}>Check all</Link>
         </div>
         <div className={styles["stat-box"]}>
             <h1>Where did you die?</h1>
             <div className={`${styles["stat-box-info"]} ${styles["stat-box-dropdown"]}`}>
                 {regionDeaths && regionDeaths.length > 0 ?
-                        regionDeaths.map(([region, regionData]) => {
-                            if(regionData.deaths > 0){
-                                return <Accordion key={region} title={region}>
-                                    <Attribute className={styles["stat-attribute"]} title={"Total Deaths"} text={`${regionData.deaths}`}/>
-                                    <Attribute className={styles["stat-attribute"]} title={"Most tried boss"} text={`${regionData.mostTried?.name}, ${regionData.mostTried?.tries}`}/>
-                                </Accordion>
-                            }
-                        }):
+                    regionDeaths.map(([region, regionData]) => {
+                        if (regionData.deaths > 0) {
+                            return <Accordion key={region} title={region}>
+                                <Attribute className={styles["stat-attribute"]} title={"Total Deaths"}
+                                           text={`${regionData.deaths}`}/>
+                                <Attribute className={styles["stat-attribute"]} title={"Most tried boss"}
+                                           text={`${regionData.mostTried?.name}, ${regionData.mostTried?.tries}`}/>
+                            </Accordion>
+                        }
+                    }) :
                     <div>You haven&apos;t tried any boss yet.</div>}
             </div>
         </div>
